8215c6be8c56a23c5d16604989888d231bdc8371
Olivier3456 <olivier.larrey@gmail.com>
2023-01-24 11:41:22 +0100

    Garderner nav mesh setup with a list of next destinations

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Transform _gardener;
+    private NavMeshAgent _gardenerAgent;
+
+    private List<Vector3> _destinationsList; 
+
+    void Start()
+    {
+        _gardenerAgent = _gardener.GetComponent<NavMeshAgent>();
+        _destinationsList = new List<Vector3>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) AddDestination();
+
+        if (Vector3.Distance(_gardener.position, _gardenerAgent.destination) < 2f && _destinationsList.Count > 0) GoToNextDestination();       
+    }
+
+    private void GoToNextDestination()
+    {
+        _gardenerAgent.destination = _destinationsList[0];
+        _destinationsList.Remove(_destinationsList[0]);
+        Debug.Log("Nouvelle destination : " + _gardenerAgent.destination);
+    }
+
+
+    private void AddDestination()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+        {
+            _destinationsList.Add(hit.point);
+            Debug.Log("Destination ajoutée à la liste : " + hit.point);
+        }
+    }
+}
